dd997b572096aae2d1d15ae3516e17874343c4d0
César Díaz Amat <cesardiazamat@gmail.com>
2023-05-22 20:59:33 +0200

    Refactorizamos cambiando el nombre del método de cocinar a hacerGachamiga

--- Test/TestGachamiga.java
+++ Test/TestGachamiga.java
@@ -8,84 +8,84 @@ public class TestGachamiga {
 
     @Test
     public void testFor1(){
-         result= g.cocinar(1);
+         result= g.hacerGachamiga(1);
         Assertions.assertEquals("1",result);
     }
 
     @Test
     public void  testFor2(){
-        result= g.cocinar(2);
+        result= g.hacerGachamiga(2);
         Assertions.assertEquals("2",result);
     }
 
     @Test
     public void testFor3(){
-        result= g.cocinar(3);
+        result= g.hacerGachamiga(3);
         Assertions.assertEquals("Gacha",result);
     }
     @Test
     public void testFor4(){
-        result= g.cocinar(4);
+        result= g.hacerGachamiga(4);
         Assertions.assertEquals("4",result);
     }
     @Test
     public void testFor5(){
-        result= g.cocinar(5);
+        result= g.hacerGachamiga(5);
         Assertions.assertEquals("Miga",result);
     }
     @Test
     public void testFor6(){
-        result= g.cocinar(6);
+        result= g.hacerGachamiga(6);
         Assertions.assertEquals("Gacha",result);
     }
     @Test
     public void testFor7(){
-        result= g.cocinar(7);
+        result= g.hacerGachamiga(7);
         Assertions.assertEquals("7",result);
     }
     @Test
     public void testFor8(){
-        result= g.cocinar(8);
+        result= g.hacerGachamiga(8);
         Assertions.assertEquals("8",result);
     }
     @Test
     public void testFor9(){
-        result= g.cocinar(9);
+        result= g.hacerGachamiga(9);
         Assertions.assertEquals("Gacha",result);
     }
     @Test
     public void testFor10(){
-        result= g.cocinar(10);
+        result= g.hacerGachamiga(10);
         Assertions.assertEquals("Miga",result);
     }
     @Test
     public void testFor11(){
-        result= g.cocinar(11);
+        result= g.hacerGachamiga(11);
         Assertions.assertEquals("11",result);
     }
     @Test
     public void testFor12(){
-        result= g.cocinar(12);
+        result= g.hacerGachamiga(12);
         Assertions.assertEquals("Gacha",result);
     }
     @Test
     public void testFor13(){
-        result= g.cocinar(13);
+        result= g.hacerGachamiga(13);
         Assertions.assertEquals("13",result);
     }
     @Test
     public void testFor14(){
-        result= g.cocinar(14);
+        result= g.hacerGachamiga(14);
         Assertions.assertEquals("14",result);
     }
     @Test
     public void testFor15(){
-        result= g.cocinar(15);
+        result= g.hacerGachamiga(15);
         Assertions.assertEquals("Gachamiga",result);
     }
     @Test
     public void testFor30(){
-        result= g.cocinar(30);
+        result= g.hacerGachamiga(30);
         Assertions.assertEquals("Gachamiga",result);
     }
 }
--- src/Gachamiga.java
+++ src/Gachamiga.java
@@ -1,5 +1,5 @@
 public class Gachamiga {
-    public String cocinar(int n){
+    public String hacerGachamiga(int n){
         if(n%3==0 && n%5==0){
             return "Gachamiga";
         }else if(n%3==0){
